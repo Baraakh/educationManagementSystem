58e292f0c9916ad34de48e579aeae54ddb1440b3
Baraa Khilo <baraakhilo04@gmail.com>
2023-09-23 12:52:08 +0400

    Creating the clsCourse class and adding find method.

--- educationManagementSystem/clsCourse.h
+++ educationManagementSystem/clsCourse.h
@@ -1,5 +1,104 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+#include "clsString.h";
+
 class clsCourse
 {
+private:
+
+	enum enMode {EmptyMode = 0, UpdateMode = 1, AddNewMode = 2};
+
+	enMode _Mode;
+	std::string _CourseCode, _CourseName, _CourseInstructor;
+
+	static clsCourse _ConvertRecordLineToObject(std::string record)
+	{
+		std::vector <std::string> vRecordData = clsString::Split(record, "#//#");
+
+		return clsCourse(enMode::UpdateMode, vRecordData[0], vRecordData[1], vRecordData[2]);
+	}
+
+	static clsCourse _GetEmptyCourseObject()
+	{
+		return clsCourse(enMode::EmptyMode, "", "", "");
+	}
+
+public:
+
+	clsCourse(enMode mode, std::string courseCode, std::string courseName, std::string courseInstructor)
+	{
+		_Mode = mode;
+		_CourseCode = courseCode;
+		_CourseName = courseName;
+		_CourseInstructor = courseInstructor;
+	}
+
+	void setCourseCode(std::string courseCode)
+	{
+		_CourseCode = courseCode;
+	}
+
+	std::string getCourseCode()
+	{
+		return _CourseCode;
+	}
+
+	__declspec(property(get = getCourseCode, put = putCourseCode)) std::string courseCode;
+
+	void setCourseName(std::string courseName)
+	{
+		_CourseName = courseName;
+	}
+
+	std::string getCourseName()
+	{
+		return _CourseName;
+	}
+
+	__declspec(property(get = getCourseName, put = setCourseName)) std::string courseName;
+
+	void setCourseInstructor(std::string courseInstructor)
+	{
+		_CourseInstructor = courseInstructor;
+	}
+
+	std::string getCourseInstructor()
+	{
+		return _CourseInstructor;
+	}
+
+	__declspec(property(get = getCourseInstructor, put = putCourseInstructor)) std::string courseInstructor;
+
+	static clsCourse find(std::string courseCode)
+	{
+		std::fstream coursesDB;
+		coursesDB.open("DB/courses.txt", std::ios::in); //read only
+
+		if (coursesDB.is_open())
+		{
+			std::string line;
+
+			while (std::getline(coursesDB, line))
+			{
+				clsCourse course = _ConvertRecordLineToObject(line);
+
+				if (course.courseCode == courseCode)
+				{
+					coursesDB.close();
+					return course;
+				}
+			}
+
+			coursesDB.close();
+		}
+
+		return _GetEmptyCourseObject();
+	}
+
 };
 
--- educationManagementSystem/educationManagementSystem.cpp
+++ educationManagementSystem/educationManagementSystem.cpp
@@ -2,31 +2,15 @@
 
 #include"clsLoginScreen.h";
 
-#include"clsDoctor.h";
-
-void print(clsDoctor doctor)
-{
-	cout << "\nInfo:";
-	cout << "\n___________________";
-	cout << "\nFirstName: " << doctor.getId();
-	cout << "\nEmail    : " << doctor.userName;
-	cout << "\nPhone    : " << doctor.password;
-	cout << "\nFirstName: " << doctor.firstName;
-	cout << "\nLastName : " << doctor.lastName;
-	cout << "\nFull Name: " << doctor.fullName();
-	cout << "\nEmail    : " << doctor.email;
-	cout << "\nPhone    : " << doctor.phone;
-	cout << "\n___________________\n";
-}
+#include "clsCourse.h";
 
 int main()
 {
 
-    //clsLoginScreen::showLoginScreen();
+    clsCourse course = clsCourse::find("CS11");
 
-    clsDoctor doctor = clsDoctor::find("d001", "d001");
+    cout << "Course name = " << course.courseName << " Code = " << course.courseCode << " - Taught by Dr: " << course.courseInstructor;
 
-	print(doctor);
-
+    system("pause>0");
     return 0;
 }
